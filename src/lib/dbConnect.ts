53888d6d86d102c160d8d87c743fbb043d1c45c4
pranav <obitouchihathesageofsixpaths@gmail.com>
2024-10-09 08:11:37 +0530

    Add MongoDB connection logic using Mongoose

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection : ConnectionObject = {} // we can keep this empty becuase is has an optional field and that is the only field it has
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("already connected to database");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {/*WHAT TO PUT HERE: SEARCH */})
+        connection.isConnected = db.connections[0].readyState;
+        console.log("Db connnected successfully");
+    } catch (error) {
+        console.log("Database connection failed", error);
+        process.exit(1);
+    }
+}
