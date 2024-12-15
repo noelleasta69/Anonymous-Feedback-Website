97ba60fe98676578c1a1b0da724b540320ace0bd
pranav <obitouchihathesageofsixpaths@gmail.com>
2024-12-15 08:57:32 +0530

    resend email added

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,5 +1,6 @@
 import { Resend } from 'resend';
-
+console.log(process.env.RESEND_API_KEY, "reesne api keey ");
 const resend = new Resend(process.env.RESEND_API_KEY);
+// console.log(resend);
 
 export default resend;
